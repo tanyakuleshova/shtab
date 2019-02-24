95423596acc798aeb2a8e30db53f90340d56ebb7
tanyakuleshova <31966962+tanyakuleshova@users.noreply.github.com>
2019-02-24 13:23:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,6 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 define('VIEWS_PATH', ROOT.DS.'views');
 
-
-
-
-
-
-
-
-
 $uri = $_SERVER['REQUEST_URI'];
 // Call dispatcher
 runApp($uri);
